b92c0e7ce3676d60f212f853c417ca7a25b6140b
Ryoma Shu <mario.rufisanto@gmail.com>
2022-06-30 20:31:05 +0700

    bootcamp controller & service init + db connection added

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
+// const logger = require('./middlewares/logger');
+const morgan = require('morgan');
 
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
+// Load Env Variables
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount Routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
