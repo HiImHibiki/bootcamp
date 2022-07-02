5ffcec69724981cc69eadd18591c0c81660390be
Ryoma Shu <mario.rufisanto@gmail.com>
2022-07-02 21:58:35 +0700

    Register User w/ Password Hashed Added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,21 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middlewares/async');
+const User = require('../models/User');
 
 // @desc    Register user
 // @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+
   res.json({
     success: true,
   });
