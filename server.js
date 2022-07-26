cbb6c2dd9e966373bd7ee86c00288fdd055bed06
Ryoma Shu <mario.rufisanto@gmail.com>
2022-07-26 12:51:39 +0700

    docgen documentation added

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use(fileupload());
 app.use(mongoSanitize());
 
 // Set security headers
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 
 // Prevent XSS attacks
 app.use(xss());
