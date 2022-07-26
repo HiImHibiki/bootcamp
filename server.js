4c863489b2d055462d9a4e13918c5065abb1b13a
Ryoma Shu <mario.rufisanto@gmail.com>
2022-07-26 12:10:40 +0700

    Rate Limitting, HPP, CORS Added

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 // const logger = require('./middlewares/logger');
 const morgan = require('morgan');
 const errorHandler = require('./middlewares/error');
@@ -49,6 +52,19 @@ app.use(helmet());
 
 // Prevent XSS attacks
 app.use(xss());
+
+// Enable CORS
+app.use(cors());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100,
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
