b12b3fedca5518beafc6766607a0e7d1bcca454e
Ryoma Shu <mario.rufisanto@gmail.com>
2022-07-26 11:56:14 +0700

    XSS Protection & Security Headers Added

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 // const logger = require('./middlewares/logger');
 const morgan = require('morgan');
 const errorHandler = require('./middlewares/error');
@@ -41,6 +43,12 @@ app.use(fileupload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
